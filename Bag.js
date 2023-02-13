be28d251c997de096a12cc46bda8f72b11877bad
Ethan <ethansteele@hotmail.co.uk>
2023-02-13 14:26:09 +0000

    finished

--- Bag.js
+++ Bag.js
@@ -1,9 +1,16 @@
 class Bag {
+    static MAX_WEIGHT = 23;
+
     constructor(weight) {
-        if(!weight){
-            throw new Error('bag needs weight')
-        } 
-        this.weight = weight
+        if (!weight) {
+            throw new Error('bag needs weight');
+        }
+
+        if (weight > Bag.MAX_WEIGHT) {
+            throw new Error(`bag weight cannot exceed ${Bag.MAX_WEIGHT}kg`);
+        }
+
+        this.weight = weight;
     }
 }
 
--- Bag.test.js
+++ Bag.test.js
@@ -3,11 +3,21 @@ const Bag = require('./Bag')
 describe('Bag class', () => {
     //bag has weight
     test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
-    })
+        const bag = new Bag(20)
+        expect(bag.weight).toBe(20)
+    });
+    
     //if bag has no weight, return error
     test('bag has no weight to return an error', () => {
         expect(() => new Bag()).toThrowError('bag needs weight')
-    })
+    });
+
+    test('should throw error if weight exceeds MAX_WEIGHT', () => {
+        expect(() => new Bag(25)).toThrowError(`bag weight cannot exceed ${Bag.MAX_WEIGHT}kg`);
+    });
+
+    test('should create a bag instance with weight equal to 20', () => {
+        const bag = new Bag(20);
+        expect(bag.weight).toBe(20);
+    });
 })
